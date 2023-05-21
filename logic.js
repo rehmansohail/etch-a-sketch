ba1c96e6ab77dcab61e69118b630946b0d389c46
Sohail Rehman <sohailmalikraja@gmail.com>
2023-05-21 14:56:48 +0530

    made the sketch board for a box of size 16 by 16

--- logic.js
+++ logic.js
@@ -14,4 +14,64 @@ function createGrid(size) {
   
   // Initialize the grid with default size
   createGrid(16);
+
   
+  /// button logic
+  // Function to change box color to white
+  function changeColor(color) {
+    const boxes = document.getElementsByClassName("box");
+
+    if (color === "rainbow") {
+      for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseover", function () {
+          const randomColor = getRandomColor();
+          this.style.backgroundColor = randomColor;
+        });
+      }
+    } else {
+      for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseover", function () {
+          this.style.backgroundColor = color;
+        });
+      }
+    }
+  }
+
+  // Function to clear the board (revert boxes to black)
+  function clearBoard() {
+    const boxes = document.getElementsByClassName("box");
+
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].style.backgroundColor = "black";
+    }
+  }
+  // for a rainbow color
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+  }
+
+   // Function to pick a color from the spectrum
+   function pickColor() {
+    const boxes = document.getElementsByClassName("box");
+    const colorPicker = document.createElement("input");
+    colorPicker.type = "color";
+
+    colorPicker.addEventListener("change", function () {
+      const pickedColor = this.value;
+
+      for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("mouseover", function () {
+          this.style.backgroundColor = pickedColor;
+        });
+      }
+    });
+
+    colorPicker.click();
+  }
