38ea24aa42b185fe3a7e371919f2b4b6751098bb
Sohail Rehman <sohailmalikraja@gmail.com>
2023-08-14 11:08:19 +0530

    added styles to buttons

--- logic.js
+++ logic.js
@@ -1,76 +1,59 @@
-const main = document.querySelector('#main_container')
+const main = document.querySelector("#main_container");
 
 //dynamically make the grid
-for(let i=0;i<32;i++){
-  let container=document.createElement('div')
-  container.classList.add('container')
-  for(let j=0;j<32;j++){
-    let box=document.createElement('div')
-    box.style.height='16px'
-    box.style.width='16px'
-    box.classList.add('box')
-    container.appendChild(box)
+for (let i = 0; i < 32; i++) {
+  let container = document.createElement("div");
+  container.classList.add("container");
+  for (let j = 0; j < 32; j++) {
+    let box = document.createElement("div");
+    box.style.height = "16px";
+    box.style.width = "16px";
+    box.classList.add("box");
+    container.appendChild(box);
   }
-  main.appendChild(container)
-
+  main.appendChild(container);
 }
 
 //to color all boxes
-const smallBoxes = document.querySelectorAll('.box');
+const smallBoxes = document.querySelectorAll(".box");
 
-// [...smallBoxes].forEach(smallBox => {
-//   smallBox.addEventListener('mouseover', () => {
-//       smallBox.style.backgroundColor='red'           //use camelCase for the color property
-//   });
-// })
-
-const buttons = document.querySelectorAll('button')
-const buttonsarr = Array.prototype.slice.call(buttons)
-buttonsarr.forEach(button=>{
-  button.addEventListener('click',(e)=>{
+const buttons = document.querySelectorAll("button");
+const buttonsarr = Array.prototype.slice.call(buttons);
+buttonsarr.forEach((button) => {
+  button.addEventListener("click", (e) => {
     let elementtext = e.target.innerText;
     // console.log(elementtext)
-    if(elementtext==='Red'){
-        [...smallBoxes].forEach(smallBox => {
-      smallBox.addEventListener('mouseover', () => {
-        smallBox.style.backgroundColor='red' 
-          
-      });
-    })
-    }else if(elementtext==='Erase'){
-      [...smallBoxes].forEach(smallBox => {
-        smallBox.addEventListener('mouseover', () => {
-          smallBox.style.backgroundColor='pink' 
-            
+    if (elementtext === "Red") {
+      [...smallBoxes].forEach((smallBox) => {
+        smallBox.addEventListener("mouseover", () => {
+          smallBox.style.backgroundColor = "red";
         });
-      })
-    }else if(elementtext==='Random'){
-      const colorButton = document.getElementById('colorButton');
-const colorPicker = document.getElementById('colorPicker');
-colorPicker.click();
+      });
+    } else if (elementtext === "Erase") {
+      [...smallBoxes].forEach((smallBox) => {
+        smallBox.addEventListener("mouseover", () => {
+          smallBox.style.backgroundColor = "pink";
+        });
+      });
+    } else if (elementtext === "Random") {
+      const colorButton = document.getElementById("colorButton");
+      const colorPicker = document.getElementById("colorPicker");
+      colorPicker.click();
 
-// Color picker change event listener
-colorPicker.addEventListener('input', (event) => {
-  const selectedColor = event.target.value;
-  [...smallBoxes].forEach(smallBox => {
-    smallBox.addEventListener('mouseover', () => {
-      smallBox.style.backgroundColor=`${selectedColor}` 
-        
-    });
-  })
-});
-    }
-    else{
+      // Color picker change event listener
+      colorPicker.addEventListener("input", (event) => {
+        const selectedColor = event.target.value;
+        [...smallBoxes].forEach((smallBox) => {
+          smallBox.addEventListener("mouseover", () => {
+            smallBox.style.backgroundColor = `${selectedColor}`;
+          });
+        });
+      });
+    } else {
       //reset everything
-      [...smallBoxes].forEach(smallBox => {
-          smallBox.style.backgroundColor='pink'
-        })
+      [...smallBoxes].forEach((smallBox) => {
+        smallBox.style.backgroundColor = "pink";
+      });
     }
-    // [...smallBoxes].forEach(smallBox => {
-    //   smallBox.addEventListener('mouseover', () => {
-          
-          
-    //   });
-    // })
-  })
-})
+  });
+});
